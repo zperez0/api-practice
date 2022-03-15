5a807823ccd4b586eae13a5b18df887dd37a64dc
Yazmin Perez <zoeperez333@gmail.com>
2022-03-15 11:26:55 -0700

    add promise, refactor code

--- src/index.js
+++ src/index.js
@@ -2,31 +2,31 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js'
 
-$(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    const city = $('#location').val();
+
+  function clearFields() {
     $('#location').val("");
+    $('.showErrors').text("");
+    $('.showHumidity').text("");
+    $('.showTemp').text("");
+    $('.showWind').val("");
+    $('.showClouds').val("");
+}
 
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        console.log(response);
-        getElements(response);
-      }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
-      $('.showWind').text(`The wind is ${response.wind.speed} mph`);
-      $('.showClouds').text(`Cloudiness is ${response.clouds.all}%`);
-    }
+  $(document).ready(function() {
+    $('#weatherLocation').click(function() {
+      let city = $('#location').val();
+      clearFields();
+      let promise = WeatherService.getWeather(city);
+      promise.then(function(response) {
+        const body = JSON.parse(response);
+        $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
+        $('.showWind').text(`The wind is ${response.wind.speed} mph`);
+        $('.showClouds').text(`Cloudiness is ${response.clouds.all}%`);
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error}`);
+      });
+    });
   });
-});
